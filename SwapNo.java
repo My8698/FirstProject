20763054f0fc582f77fbc7a4f59cfa0a454c8dea
My8698 <112363755+My8698@users.noreply.github.com>
2022-10-21 16:11:16 +0530

    Update and rename swap.java to SwapNo.java

--- SwapNo.java
+++ SwapNo.java
@@ -1,12 +1,10 @@
 package com.bridgelabz.basics;
-
-public class swap {
+public class SwapNo {
     public static void main(String[] args) {
         int x=10,y=20;
         System.out.println("Numbers before swapping:");
         System.out.println("x= "+x);
         System.out.println("y= "+y);
-
         int temp=x;
         x=y;
         y=temp;
