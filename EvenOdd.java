1078be26c8f7d1f750487a8ba5cde80feff575e8
My8698 <112363755+My8698@users.noreply.github.com>
2022-10-21 15:55:29 +0530

    Update and rename evenodd.java to EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,12 +1,9 @@
 package com.bridgelabz.basics;
-
-public class evenodd {
+public class EvenOdd {
     public static void main(String[] args) {
         int no=33;
-
         if (no % 2 == 0){
             System.out.println("Number is Even");
-
         }
         else{
             System.out.println("Number is Odd");
