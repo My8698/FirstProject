ce631e1c52ea4be4f837a884fa36f48c66453c17
My8698 <112363755+My8698@users.noreply.github.com>
2022-10-21 15:58:32 +0530

    Update and rename flipcoin.java to FlipCoin.java

--- FlipCoin.java
+++ FlipCoin.java
@@ -1,8 +1,6 @@
 package com.bridgelabz.basics;
-
 import java.util.Scanner;
-
-public class flipcoin {
+public class FlipCoin {
     public static void main(String[] args) {
         int head = 0;
         int tail = 0;
@@ -11,16 +9,13 @@ public class flipcoin {
         Scanner scn = new Scanner(System.in);
         System.out.println("Times will be coin flipped?");
         int flip = scn.nextInt();
-
         while (counter <= flip) {
             randnum = Math.random();
             System.out.print(counter + "\t" + randnum);
-
-            if (randnum < .5) {
+                if (randnum < .5) {
                 head++;
                 System.out.println("\t Head");
-
-            } else {
+                } else {
                 tail++;
                 System.out.println("\t Tail");
             }
@@ -28,8 +23,6 @@ public class flipcoin {
         }
         System.out.println();
         System.out.println("Number of heads=" + head);
-
         System.out.println("Number of tailss=" + tail);
-
-    }
+      }
 }
