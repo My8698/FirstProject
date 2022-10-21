e2b74ed83e5ef537cc8d650d2474902d0d2cb860
My8698 <112363755+My8698@users.noreply.github.com>
2022-10-21 16:01:59 +0530

    Update and rename leapyear.java to LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -1,24 +1,17 @@
 package com.bridgelabz.basics;
-
 import java.util.Scanner;
-
-public class leapyear {
+public class LeapYear {
     public static void main(String[] args) {
         int year;
         Scanner scn=new Scanner(System.in);
                 year=scn.nextInt();
-
-        if ((year % 400 == 0)
+            if ((year % 400 == 0)
             ||((year % 4 == 0) && (year % 100 != 0)))
         {
             System.out.println(year+":Leap Year");
-
         }
         else {
             System.out.println(year+":Non Leap Year");
         }
-
     }
-
-
 }
